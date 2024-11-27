aff0f7cbe45346a08cf7cae767b80b8b3fad9fc0
Сиздин Атыңыз <email@.com>
2024-11-28 00:17:53 +0300

    section 2 бутту

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ document.querySelector('.img').addEventListener('click', function () {
     }
   });
    
+document.querySelector('.img2').addEventListener('click', function () {
+    const menu = document.querySelector('.dropdown-menu');
+    if (menu.style.display === 'none' || menu.style.display === '') {
+      menu.style.display = 'block'; 
+    } else {
+      menu.style.display = 'none';
+    }
+  });
+   
