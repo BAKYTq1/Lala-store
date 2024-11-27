92aa86d10470b493fc46aecef53123033f09492d
Сиздин Атыңыз <email@.com>
2024-11-27 23:44:48 +0300

    2-section бутту

---
+++ app.js
@@ -0,0 +1,9 @@
+document.querySelector('.img').addEventListener('click', function () {
+    const menu = document.querySelector('.dropdown-menu');
+    if (menu.style.display === 'none' || menu.style.display === '') {
+      menu.style.display = 'block'; 
+    } else {
+      menu.style.display = 'none';
+    }
+  });
+   
