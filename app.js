a9a25fd30d8f75d47f1b197db4851497947e869f
Сиздин Атыңыз <email@.com>
2024-12-01 13:51:33 +0300

    section 2 бутту

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 document.querySelector('.img').addEventListener('click', function () {
-    const menu = document.querySelector('.dropdown-menu');
+    const menu = document.querySelector('.input');
     if (menu.style.display === 'none' || menu.style.display === '') {
       menu.style.display = 'block'; 
     } else {
       menu.style.display = 'none';
     }
   });
-   
 document.querySelector('.img2').addEventListener('click', function () {
-    const menu = document.querySelector('.dropdown-menu');
+    const menu = document.querySelector('.input2');
+    if (menu.style.display === 'none' || menu.style.display === '') {
+      menu.style.display = 'block'; 
+    } else {
+      menu.style.display = 'none';
+    }
+  });
+document.querySelector('.img3').addEventListener('click', function () {
+    const menu = document.querySelector('.input3');
+    if (menu.style.display === 'none' || menu.style.display === '') {
+      menu.style.display = 'block'; 
+    } else {
+      menu.style.display = 'none';
+    }
+  });
+document.querySelector('.img4').addEventListener('click', function () {
+    const menu = document.querySelector('.input4');
+    if (menu.style.display === 'none' || menu.style.display === '') {
+      menu.style.display = 'block'; 
+    } else {
+      menu.style.display = 'none';
+    }
+  });
+document.querySelector('.img5').addEventListener('click', function () {
+    const menu = document.querySelector('.input5');
     if (menu.style.display === 'none' || menu.style.display === '') {
       menu.style.display = 'block'; 
     } else {
@@ -16,3 +39,4 @@ document.querySelector('.img2').addEventListener('click', function () {
     }
   });
    
+  
